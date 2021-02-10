0caa23fe0b02e39802e3058608a7379ff20f7977
xinglin2016 <lixinglin0829@gmail.com>
2021-02-09 21:31:51 -0500

    add on file scss

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,11 +7,8 @@ class QuestionsController < ApplicationController
     if params[:question] == 'I am going to work'
       @answer = 'Great!'
     elsif params[:question].to_s.include?('?')
-    # elsif @question.to_s[-1] == '?'
-      puts @question
       @answer = 'Silly question, get dressed and go to work!'
     else
-      puts @question
       @answer = "I don't care, get dressed and go to work!"
     end
   end
