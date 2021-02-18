f78edc09c6f33da3b9f2304991baa88a25c087d3
xinglin2016 <lixinglin0829@gmail.com>
2021-02-17 20:08:51 -0500

    modify method in controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,9 +4,12 @@ class QuestionsController < ApplicationController
 
   def answer
     # raise
-    if params[:question] == 'I am going to work'
+    @question = params[:question]
+    if @question.blank?
+      @answer = "I cannot hear you!"
+    elsif @question == 'I am going to work'
       @answer = 'Great!'
-    elsif params[:question].to_s.include?('?')
+    elsif @question.ends_with?('?')
       @answer = 'Silly question, get dressed and go to work!'
     else
       @answer = "I don't care, get dressed and go to work!"
